49c35f60edeb31bae485d6cd14161912243a49d9
Victor-Hugo1 <victor.hsouza@sptech.school>
2025-05-22 23:54:10 -0300

    alteracoes java

--- ELEVA/src/main/java/grupo9/eleva/bdpath/ConexaoBD.java
+++ ELEVA/src/main/java/grupo9/eleva/bdpath/ConexaoBD.java
@@ -1,42 +1,21 @@
 package grupo9.eleva.bdpath;
 
-import grupo9.eleva.ConfigurationVariable;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class ConexaoBD {
 
     private final DataSource dataSource;
-    private final String url = ConfigurationVariable.get("URL");
-    private final String password = ConfigurationVariable.get("PASSWORD");
 
     public ConexaoBD() {
         BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(url);
+        basicDataSource.setUrl("jdbc:mysql://mysql:3306/eleva?useSSL=false&serverTimezone=GMT-3&allowPublicKeyRetrieval=true");
         basicDataSource.setUsername("root");
-        basicDataSource.setPassword(password);
+        basicDataSource.setPassword("sptech");
 
         System.out.println("Tentando conectar ao banco de dados...");
-
-
-        while (true) {
-            try (Connection connection = basicDataSource.getConnection()) {
-                System.out.println(" Conexão bem-sucedida com o banco!");
-                break;
-            } catch (SQLException e) {
-                System.out.println("Banco não está pronto. Tentando novamente em 5 segundos...");
-                try {
-                    Thread.sleep(5000);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }
-
         this.dataSource = basicDataSource;
     }
 
--- ELEVA/src/main/java/grupo9/eleva/s3connection/ConnectorS3.java
+++ ELEVA/src/main/java/grupo9/eleva/s3connection/ConnectorS3.java
@@ -1,36 +1,15 @@
 package grupo9.eleva.s3connection;
 
-import grupo9.eleva.ConfigurationVariable;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 public class ConnectorS3 {
 
-    private final AwsCredentialsProvider credentialsProvider;
-
-    public ConnectorS3() {
-        String accessKey = ConfigurationVariable.get("aws_access_key_id");
-        String secretKey = ConfigurationVariable.get("aws_secret_access_key");
-        String sessionToken = ConfigurationVariable.get("aws_session_token");
-
-        if (sessionToken != null && !sessionToken.isEmpty()) {
-            AwsSessionCredentials sessionCredentials = AwsSessionCredentials.create(
-                    accessKey, secretKey, sessionToken);
-            this.credentialsProvider = StaticCredentialsProvider.create(sessionCredentials);
-        } else {
-            AwsBasicCredentials basicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
-            this.credentialsProvider = StaticCredentialsProvider.create(basicCredentials);
-        }
-    }
-
     public S3Client getS3Client() {
         return S3Client.builder()
-                .region(Region.US_EAST_1) // Altere se necessário
-                .credentialsProvider(credentialsProvider)
+                .region(Region.of("us-east-1"))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
